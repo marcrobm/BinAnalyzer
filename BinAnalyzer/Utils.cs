ca22493f426206c0d8b3301dd7ef34d3ce5596c2
marcrobm <50637739+marcrobm@users.noreply.github.com>
2022-09-26 10:06:00 +0200

    documented buildIntervalls

--- BinAnalyzer/Utils.cs
+++ BinAnalyzer/Utils.cs
@@ -9,9 +9,9 @@ namespace BinAnalyzer
         /// <summary>
         /// Creates count disjoint intervalls between min and max which cover all values between and including min,max
         /// </summary>
-        /// <param name="min"></param>
-        /// <param name="max"></param>
-        /// <param name="count"></param>
+        /// <param name="min"> The start of the range in which to generate Intervalls</param>
+        /// <param name="max"> The end f the Range in which to generate Intervalls</param>
+        /// <param name="count"> The number of intervalls to place in the range min->max</param>
         /// <returns>number,start,end</returns>
         public static IEnumerable<Tuple<ulong,ulong, ulong>> buildIntervals(ulong min, ulong max, ulong count)
         {
@@ -19,21 +19,21 @@ namespace BinAnalyzer
             var res = new List<Tuple<ulong,ulong, ulong>>();
             ulong step = (max - min) / count;
             if (step <= 0) return null;
-            ulong mod = (max - min) % count;
+            ulong mod = (max - min) % count; // ensured that the entire range will be covered
             ulong begin = min;
             ulong end;
-            for (ulong i = 0; i < count; i++)
+            for (ulong intervall = 0; intervall < count; intervall++)
             {
                 end = begin + step;
                 if (mod > 0) {
                     mod--;
                     end++;
                 }
-                var val = new Tuple<ulong,ulong, ulong>(i,begin, end-1);
+                var val = new Tuple<ulong,ulong, ulong>(intervall,begin, end-1);
                 res.Add(val);
                 begin = end;
             }
-            return res;//should be unreachable
+            return res;
         }
     }
 }
